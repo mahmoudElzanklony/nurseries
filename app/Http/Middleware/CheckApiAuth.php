95b95460ecdb4c08de132b3f0e17d1a5c726c04c
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-09-27 21:35:13 +0300

    add

--- app/Http/Middleware/CheckApiAuth.php
+++ app/Http/Middleware/CheckApiAuth.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Http\traits\messages;
 use Closure;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 class CheckApiAuth extends BaseMiddleware
 {
@@ -17,11 +18,17 @@ class CheckApiAuth extends BaseMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $token = $request->header('token');
-        $request->headers->set('token',(string)$token,true);
-        $request->headers->set('Authorization','Bearer '.$token,true);
-        try {
+        if(request()->hasHeader('token')){
+            $token = $request->header('token');
+            $request->headers->set('token',(string)$token,true);
+            $request->headers->set('Authorization','Bearer '.$token,true);
             $user = $this->auth->parseToken()->authenticate($request);
+        }else{
+            $token = JWTAuth::parseToken()->authenticate();
+            $user = $token;
+        }
+        try {
+
             if($user == false){
                 return messages::error_output(['invalid credential']);
             }
