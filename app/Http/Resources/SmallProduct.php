5f4a2e03861e1b65046fd97ea8bc8a09b417bd0d
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-10-22 05:01:31 +0300

    add

--- app/Http/Resources/SmallProduct.php
+++ app/Http/Resources/SmallProduct.php
@@ -20,7 +20,7 @@ class SmallProduct extends JsonResource
           'name'=>$this->product->{app()->getLocale().'_name'},
           'price'=>$this->when(true,function (){
                 $price = $this->price;
-                $features = orders_items_features::query()->where('order_item_id','=',$this->id)->count('price') ?? 0;
+                $features = orders_items_features::query()->where('order_item_id','=',$this->id)->sum('price') ?? 0;
                 return $price + $features;
           }),
           'created_at'=>$this->created_at,
