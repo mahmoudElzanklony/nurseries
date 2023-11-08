971959c035ee161f4e78638a96e6de1b40a45f3a
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-11-08 02:11:42 +0200

    add

--- app/Http/Resources/ProductAnswersResource.php
+++ app/Http/Resources/ProductAnswersResource.php
@@ -14,16 +14,21 @@ class ProductAnswersResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
+        $arr =  [
           'id'=>$this->id,
           'category_heading_questions_data_id'=>$this->category_heading_questions_data_id,
           'answer'=>$this->{app()->getLocale().'_answer'},
-        //  'image'=>ImagesResource::make($this->question->image),
           'created_at'=>$this->created_at,
           'question'=>$this->when(method_exists($this,'whenLoaded') && method_exists($this,'relationLoaded'),function (){
               return ProductQuestionResource::make($this->whenLoaded('question'));
           }),
 
         ];
+        try{
+            $arr['image'] = ImagesResource::make($this->question->image);
+        }catch (\Throwable $e){
+
+        }
+        return $arr;
     }
 }
