0140f00147fa5dcf96ca512879cf3316b328bec9
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-11-08 02:22:45 +0200

    add

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -43,7 +43,7 @@ class OrdersController extends Controller
         // check if this of any these products any one that has no delivery way to default client address
         $check_err_delivery = $order_repo->check_delivery_products($data['items']);
         if($check_err_delivery['error'] > 0){
-            return messages::error_output($seller->username.' '.trans('keywords.dont_support_delivery_product').' '.$check_err_delivery['product_name'].trans('keywords.to_default_address'),401);
+            return messages::error_output($seller->username.' '.trans('keywords.dont_support_delivery_product').' ( '.$check_err_delivery['product_name'].' ) '.trans('keywords.to_default_address'),401);
         }
         if($order_repo->validate_payment_info($data['payment_data'])['status'] == true){
             // the visa is okay now
