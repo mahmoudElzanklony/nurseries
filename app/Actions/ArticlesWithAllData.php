c28f31abaea450a7ca6b0c96a4dabe594475ee24
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-11-14 01:00:42 +0200

    add

--- app/Actions/ArticlesWithAllData.php
+++ app/Actions/ArticlesWithAllData.php
@@ -13,6 +13,9 @@ class ArticlesWithAllData
             ->when(GetAuthenticatedUser::get_info() != null , function ($e){
                 $e->with('like');
             })
+            ->when(GetAuthenticatedUser::get_info() != null && auth()->user()->role->name == 'seller' , function ($e){
+                $e->where('user_id','=',auth()->id());
+            })
             ->with(['user','category','images','comments'=>function($e){
                 $e->with('user');
             },'seen'])->withCount('likes')
