1010be98dba51fd0d6206a569441853468913681
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-11-24 00:25:01 +0200

    add

--- app/Http/Resources/CouponRessource.php
+++ app/Http/Resources/CouponRessource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CouponRessource extends JsonResource
@@ -23,6 +24,7 @@ class CouponRessource extends JsonResource
           'discount'=>$this->discount,
           'end_date'=>$this->end_date,
           'created_at'=>$this->created_at,
+          'status'=>$this->end_date == null ? true:(Carbon::parse($this->end_date) >= Carbon::now() ? true:false),
           'statistics'=>CouponProductsResource::collection($this->whenLoaded('order_items')),
           'products'=>TinyProductResource::collection($this->whenLoaded('products')),
           'users'=>CouponProductsResource::collection($this->whenLoaded('users')),
