2864d74ae49bfeff84601fd65f12322a29e04b45
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-03 01:27:17 +0200

    add

--- app/Http/Resources/CategoryHeadingQuestionsDataResource.php
+++ app/Http/Resources/CategoryHeadingQuestionsDataResource.php
@@ -18,7 +18,7 @@ class CategoryHeadingQuestionsDataResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->{app()->getLocale().'_name'},
             'type'=>$this->type,
-            'selections'=>$this->when($this->type == 'selection',function (){
+            'selections'=>$this->when($this->type == 'selection' || $this->type == 'select',function (){
                 return SelectionResource::collection($this->selections);
             }),
             'image'=>ImagesResource::make($this->whenLoaded('image')),
