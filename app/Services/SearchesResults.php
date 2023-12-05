17be3f61b4ef73693ccb7447dbe154a43a944a59
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-06 01:09:39 +0200

    add

--- app/Services/SearchesResults.php
+++ app/Services/SearchesResults.php
@@ -11,7 +11,7 @@ class SearchesResults
     public static function last_searches($type){
         $data = searches::query()
             ->where('user_id','=',auth()->id())
-            ->where('type','=',$type)->orderBy('updated_at','DESC')->paginate(15);
+            ->where('type','=',$type)->orderBy('id','DESC')->paginate(15);
         return $data;
     }
     public static function added_to_search($item_id,$type){
@@ -21,15 +21,13 @@ class SearchesResults
             'type'=>$type,
         ])->first();
         if($output != null){
-            $output->updated_at = date('Y-m-d H:i:s');
-            $output->save();
-        }else{
-            searches::query()->create([
-                'user_id'=>auth()->id(),
-                'item_id'=>$item_id,
-                'type'=>$type,
-            ]);
+            $output->delete();
         }
+        searches::query()->create([
+            'user_id'=>auth()->id(),
+            'item_id'=>$item_id,
+            'type'=>$type,
+        ]);
 
     }
 }
