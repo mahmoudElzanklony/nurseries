1d7e7ef96498685fea7e57ea5acdd5d9c3158d6c
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-07 04:15:54 +0200

    add

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -44,7 +44,6 @@ class UserResource extends JsonResource
           'store_info'=>UserStoreInfoResource::make($this->whenLoaded('store_info')),
           'complete_data'=>$this->when(isset($this->role) && $this->role->name == 'seller',function(){
              $store = users_store_info::query()->where('user_id',$this->id)->first();
-             dd($store);
              if($store != null){
                  return true;
              }else{
