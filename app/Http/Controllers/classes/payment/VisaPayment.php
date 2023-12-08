5cf9dac9b566576fd2acca85a6910ee680ce854a
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-08 02:04:57 +0200

    add

--- app/Http/Controllers/classes/payment/VisaPayment.php
+++ app/Http/Controllers/classes/payment/VisaPayment.php
@@ -23,7 +23,7 @@ class VisaPayment implements IPayment
     }
 
     public function check_payment_related_to_user($data){
-        dd(auth()->user());
+        echo $data['id'];
         $user_visa = users_visa::query()
             ->where('user_id','=',auth()->id())
             ->where('id','=',$data['id'])->first();
