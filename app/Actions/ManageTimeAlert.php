f2ed5d5a46a818e4ac92118bc209f32a8dc3632b
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-14 23:14:14 +0200

    add

--- app/Actions/ManageTimeAlert.php
+++ app/Actions/ManageTimeAlert.php
@@ -16,6 +16,7 @@ class ManageTimeAlert
         }else{
             $now = Carbon::create($last_time);
         }
+        dd($now);
         if ($time_type == 'minute') {
             $time = $now->addMinutes($time_number);
         } else if ($time_type == 'hour') {
