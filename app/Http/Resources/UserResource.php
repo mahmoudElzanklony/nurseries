a85eadc242ece89971056cbfdf06f3181f65b2fc
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-15 22:53:59 +0200

    add

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -34,7 +34,7 @@ class UserResource extends JsonResource
               return UserAddressesResource::make($this->default_address);
           }),
           'farm_address'=>$this->when(isset($this->farm_address) && $this->farm_address->address != null,function (){
-              return UserAddressesResource::make($this->whenLoaded($this->farm_address->address));
+              return UserAddressesResource::make($this->farm_address->address);
           }),
           'token'=>isset($this->token) ? $this->token : null,
           'article_permission'=>isset($this->article_permission) ? true:false,
