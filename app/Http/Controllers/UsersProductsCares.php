efab3925461aa24563fca5d8f478dcbadd22ea69
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-18 03:04:25 +0200

    ad

--- app/Http/Controllers/UsersProductsCares.php
+++ app/Http/Controllers/UsersProductsCares.php
@@ -64,7 +64,7 @@ class UsersProductsCares extends Controller
             ->where('product_id','=',$product_id)
             ->where('type','=','seller')
             ->get();
-        dd(sizeof($product_cares),$product_cares);
+
         foreach($product_cares as $care){
             $time = ManageTimeAlert::manage($care->time_number,$care->time_type,null);
             dd($time->format('Y-m-d H:i:s'),$care->time_number,$time);
