194d4adb6b365f08b18dc748de4d7d6db4e1aedd
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-18 21:43:45 +0200

    add

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -68,7 +68,7 @@ class ProductResource extends JsonResource
                        return $e->rate_product_info >= 4;
                    })->map(function ($e){
                        return $e->rate_product_info;
-                   })->sum();
+                   })->sum() / sizeof($this->rates);
                }else{
                    return 0;
                }
