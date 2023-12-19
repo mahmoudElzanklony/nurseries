54ecd8fc738852b50dfba9f74c8709a3e7ba44cb
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-19 02:20:13 +0200

    ad  some data

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -61,9 +61,8 @@ class OrdersController extends Controller
                 }
             }
             DB::commit();
-            $order_repo->order->has_coupon = ($order_repo->order->has_coupon == 0 ? false:true);
-            $order_repo->order->seller_profit = false;
-            return messages::success_output(trans('messages.order_done_successfully'),$order_repo->order);
+
+            return messages::success_output(trans('messages.order_done_successfully'),OrdersWithAllData::get()->find($order_repo->order->id));
         }else{
             return messages::error_output('بيانات الفيزا الخاصه بك خاطئة يرجي مراجعتها من فضلك');
         }
