20d3d4f2bd08dd478e2c95d6dd07b1423c493f81
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-19 02:13:01 +0200

    ad  some data

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -61,7 +61,7 @@ class OrdersController extends Controller
                 }
             }
             DB::commit();
-            $order_repo->order->has_coupon = $order_repo->order->has_coupon == 0 ? false:true;
+            $order_repo->order->has_coupon = ($order_repo->order->has_coupon == 0 ? false:true);
             $order_repo->order->seller_profit = false;
             return messages::success_output(trans('messages.order_done_successfully'),$order_repo->order);
         }else{
