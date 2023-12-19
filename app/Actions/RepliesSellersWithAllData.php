c5d411a7663e95089e4dbb17d9c2f123f924aae7
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-19 22:36:05 +0200

    update custom

--- app/Actions/RepliesSellersWithAllData.php
+++ app/Actions/RepliesSellersWithAllData.php
@@ -9,9 +9,16 @@ use App\Models\custom_orders_sellers;
 class RepliesSellersWithAllData
 {
     public static function get(){
-        return custom_orders_sellers::query()->with('order')->whereHas('order',function($e){
-            $e->where('user_id','=',auth()->id());
-        })->with(['seller','reply'=>function($e){
+        return custom_orders_sellers::query()->with('order')
+            ->when(auth()->user()->role->name == 'client' || auth()->user()->role->name == 'company' ,function($e){
+                $e->whereHas('order',function($e){
+                    $e->where('user_id','=',auth()->id());
+                });
+            })
+            ->when(auth()->user()->role->name == 'seller',function($e){
+                $e->where('seller_id','=',auth()->id());
+            })
+            ->with(['seller','reply'=>function($e){
                 $e->with('images');
             }])->when(request()->filled('name'),function($e){
                 $e->whereHas('order',function($q){
