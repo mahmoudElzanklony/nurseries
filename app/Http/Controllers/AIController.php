91e685af7c7fd5564bd8875b8fe05661c07a5d49
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2023-12-26 18:51:33 +0200

    adding

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -17,11 +17,7 @@ class AIController extends Controller
 
         // Create a new OpenAI client.
         $client = new OpenAi(env('openai'));
-        $prompt = "add a variety of lush and vibrant plants, each with unique colors, shapes,
-        and sizes. Ensure that the plants are seamlessly integrated into the scene and appear natural in their placement.
-        The overall composition should evoke a sense of tranquility and aesthetics. Please include a diverse selection of plants to offer the client a range of options.
-        The lighting should be soft and natural, complementing the overall ambiance. The final image should be of the highest quality,
-        suitable for presentation to the client for their selection ";
+        $prompt = "add a variety of lush and vibrant plants, each with unique colors, shapes, and sizes. Ensure that the plants are seamlessly integrated into the scene and appear natural in their placement.The overall composition should evoke a sense of tranquility and aesthetics. Please include a diverse selection of plants to offer the client a range of options.The lighting should be soft and natural, complementing the overall ambiance. The final image should be of the highest quality,suitable for presentation to the client for their selection ";
 
         if(request()->has('questions')){
             foreach(request('questions') as $q){
@@ -29,7 +25,7 @@ class AIController extends Controller
                 if($question != null){
                     $prompt .= 'and '.$question->en_name.' is '.$q['answer'].' ';
                     if($question->en_name == 'plant place'){
-                        if($q['answer'] == 'inner house'){
+                        if($q['answer'] == 'outside house'){
                             $trees = ['Shade trees ','Heat tolerant trees ','Evergreen trees ','Ornamental trees and plants ','Flowering trees ','Deciduous trees ','Fruit trees '
                                 ,'Seasonal flowers and plants ','Fragrant trees and plants ','Aromatic plants ','Flowering plants ','Climbing plants ','Rare plants '];
 
