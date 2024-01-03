353785d8a52d08263a3fdcedd84459453a791ede
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-03 02:17:28 +0200

    add

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -116,7 +116,7 @@ class AIController extends Controller
                 "image_strength"=> 0.35,
                 "init_image_mode"=> "IMAGE_STRENGTH",
                 "text_prompts[0][text]" => $prompt,
-                "cfg_scale" => 20,
+                "cfg_scale" => 5,
                 "clip_guidance_preset" => "FAST_BLUE",
                 "sampler" => "K_DPM_2_ANCESTRAL",
                 "samples" => 6,
