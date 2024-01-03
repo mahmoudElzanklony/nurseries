b23e972f8a4d3680837c03b7fce03a74a6c2ee7e
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-04 00:59:30 +0200

    upadte commit

--- app/Actions/CheckPlaceMapLocation.php
+++ app/Actions/CheckPlaceMapLocation.php
@@ -38,8 +38,8 @@ class CheckPlaceMapLocation
                 foreach ($result['address_components'] as $address_component) {
                     if (in_array('locality', $address_component['types']) && in_array($address_component['long_name'],$cities_en_english)) {
                         $result =  $deliveries->find(function ($e) use ($address_component){
-                            dd($e['city']);
-                            return $e['city']['en_name'] == $address_component['long_name'];
+                            dd($e);
+                         //   return $e['city']['en_name'] == $address_component['long_name'];
                         });
                         break;
                     }
