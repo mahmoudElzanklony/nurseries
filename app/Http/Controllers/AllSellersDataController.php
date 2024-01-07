2d5fcf16e769dca61b1ee438bbed34da172bf58c
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-07 19:52:27 +0200

    update

--- app/Http/Controllers/AllSellersDataController.php
+++ app/Http/Controllers/AllSellersDataController.php
@@ -60,7 +60,7 @@ class AllSellersDataController extends Controller
                 return messages::error_output('لا يوجد ردود');
             }
         }
-        return custom_orders_sellers::query()->with('reply')->whereRelation('reply', 'client_reply', '=','pending')->get();
+        return custom_orders_sellers::query()->whereRelation('reply', 'client_reply', '=','pending')->get();
         $output = app(Pipeline::class)
             ->send($data)
             ->through([
