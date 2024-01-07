969eb7785a60c544c36107f9b47cbc80783d8b0d
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-07 19:47:50 +0200

    update

--- app/Actions/RepliesSellersWithAllData.php
+++ app/Actions/RepliesSellersWithAllData.php
@@ -13,20 +13,7 @@ class RepliesSellersWithAllData
             ->whereHas('reply',function($r){
                 $r->whereRaw('custom_orders_sellers_replies.client_reply = "pending" ');
             })
-            ->when(auth()->user()->role->name == 'client' || auth()->user()->role->name == 'company' ,function($e){
-                $e->whereHas('order',function($e){
-                    $e->where('user_id','=',auth()->id());
-                });
-            })
-            ->when(auth()->user()->role->name == 'seller' ,function($e){
-                $e->where('seller_id','=',auth()->id());
-            })
-            ->with(['seller','reply'=>function($e){
-                $e->with('images');
-            }])->when(request()->filled('name'),function($e){
-                $e->whereHas('order',function($q){
-                   $q->where('name','LIKE','%'.request('name').'%');
-                });
-        })->orderBy('id','DESC');
+
+        ->orderBy('id','DESC');
     }
 }
