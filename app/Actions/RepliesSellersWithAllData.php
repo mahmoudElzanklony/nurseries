b0272323f537c28c429943874e454718aee2f92f
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-07 19:07:20 +0200

    update

--- app/Actions/RepliesSellersWithAllData.php
+++ app/Actions/RepliesSellersWithAllData.php
@@ -14,7 +14,7 @@ class RepliesSellersWithAllData
                 $e->whereHas('order',function($e){
                     $e->where('user_id','=',auth()->id());
                 })->whereHas('reply',function($r){
-                    $r->where('product_price','>', '10');
+                    $r->where('product_price','<', '10');
                 });
             })
             ->when(auth()->user()->role->name == 'seller' ,function($e){
