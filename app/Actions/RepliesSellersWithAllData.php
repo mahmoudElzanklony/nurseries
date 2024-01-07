ac77a88169e385a1de99dd668c7286475c6c817c
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-07 18:51:52 +0200

    update

--- app/Actions/RepliesSellersWithAllData.php
+++ app/Actions/RepliesSellersWithAllData.php
@@ -5,16 +5,20 @@ namespace App\Actions;
 
 
 use App\Models\custom_orders_sellers;
+use Illuminate\Support\Facades\DB;
 
 class RepliesSellersWithAllData
 {
     public static function get(){
         return custom_orders_sellers::query()->with('order')
             ->when(auth()->user()->role->name == 'client' || auth()->user()->role->name == 'company' ,function($e){
-                $e->whereHas('order',function($e){
+                $e->whereExists(function ($query) {
+                    $query->select(DB::raw(1))
+                        ->from('custom_orders_sellers_replies')
+                        ->whereRaw('client_reply = "pending"')
+                        ->whereColumn('custom_orders_sellers_replies.custom_orders_seller_id ', 'custom_orders_sellers.id');
+                })->whereHas('order',function($e){
                     $e->where('user_id','=',auth()->id());
-                })->whereHas('reply',function($e){
-                    $e->where('client_reply','=','pending');
                 });
             })
             ->when(auth()->user()->role->name == 'seller' ,function($e){
