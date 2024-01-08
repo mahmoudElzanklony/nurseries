02232c1af2be0db4c9f7f041183c663e90b13eb3
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-08 17:14:11 +0200

    add

--- app/Filters/products/QuestionsFilter.php
+++ app/Filters/products/QuestionsFilter.php
@@ -10,8 +10,8 @@ class QuestionsFilter
             $questions_answers = collect(request('questions'))->map(fn($e)=> $e['answer']);
             return $next($request)
                 ->whereHas('answers',function($e) use ($questions_ids,$questions_answers){
-                    $e->whereIn('category_heading_questions_data_id ',$questions_ids)
-                      ->whereIn('ar_answer ',$questions_answers);
+                    $e->whereIn('products_questions_answers.category_heading_questions_data_id ',$questions_ids)
+                      ->whereIn('products_questions_answers.ar_answer ',$questions_answers);
                 });
         }
         return $next($request);
