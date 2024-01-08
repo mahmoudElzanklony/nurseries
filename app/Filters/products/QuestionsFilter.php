cab217712ebc5213047ec35a624d7492acbf1682
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-08 17:15:50 +0200

    add

--- app/Filters/products/QuestionsFilter.php
+++ app/Filters/products/QuestionsFilter.php
@@ -10,7 +10,7 @@ class QuestionsFilter
             $questions_answers = collect(request('questions'))->map(fn($e)=> $e['answer']);
             return $next($request)
                 ->whereHas('answers',function($e) use ($questions_ids,$questions_answers){
-                    $e->whereIn('category_heading_questions_data_id ',$questions_ids)
+                    $e->whereIn('category_heading_questions_data_id',$questions_ids)
                       ->whereIn('ar_answer ',$questions_answers);
                 });
         }
