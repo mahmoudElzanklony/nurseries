821d80ed20c33c48b875b87074938a46f3aa0e00
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-09 22:52:52 +0200

    update order api

--- app/Http/Resources/RateResource.php
+++ app/Http/Resources/RateResource.php
@@ -16,6 +16,7 @@ class RateResource extends JsonResource
     {
         return [
           'id'=>$this->id,
+          'product_id'=>$this->product_id,
           'comment'=>$this->comment,
           'rate_product_info'=>$this->rate_product_info,
           'rate_product_services'=>$this->rate_product_services,
