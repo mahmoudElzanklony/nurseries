c9118c57c741edb4438d82c4034a674f99d4a923
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-09 22:50:29 +0200

    update order api

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -43,6 +43,7 @@ class ProductResource extends JsonResource
             }
         }else{
             $rate_bars = [];
+            $seller_avg_rate = [];
         }
 
         return [
@@ -95,7 +96,7 @@ class ProductResource extends JsonResource
                 return round($this->rates->avg('rate_product_info'),2);
             }),
             'avg_rates_seller'=>$this->when(str_contains(request()->fullUrl(), 'orders') == false,function() use ($seller_avg_rate){
-               dd(str_contains(request()->fullUrl(), 'orders'));
+
                return round(($seller_avg_rate['avg_services']+$seller_avg_rate['avg_delivery'])/2,2);
             }),
             'is_following'=>auth()->check() && followers::query()->where('user_id',auth()->id())->where('following_id',$this->user_id)->first() != null ? true:false,
