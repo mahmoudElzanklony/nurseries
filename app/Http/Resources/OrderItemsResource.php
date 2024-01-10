f51b3d1aa7dfcada312f0e5b4291ee1c388900b3
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-10 19:59:13 +0200

    add order info

--- app/Http/Resources/OrderItemsResource.php
+++ app/Http/Resources/OrderItemsResource.php
@@ -20,7 +20,22 @@ class OrderItemsResource extends JsonResource
           'rate'=>RateResource::make($this->whenLoaded('rate')),
           'features'=>OrderItemsFeaturesResource::collection($this->whenLoaded('features')),
           'quantity'=>$this->quantity,
-          'price'=>doubleval($this->price),
+          'prices_info'=>$this->when(isset($this->payment) && $this->payment != null , function(){
+              $tax_percen = $this->payment->tax;
+
+
+             $all_price_with_tax = round($this->price, 2);
+             $all_price_without_tax = round($this->price / (1+$tax_percen/100),2);
+             $price_item_with_tax = round($this->price / $this->quantity, 2);
+             $price_item_without_tax = round($all_price_without_tax / $this->quantity, 2);
+             return [
+               'all_price_with_tax'=>$all_price_with_tax,
+               'all_price_without_tax'=>$all_price_without_tax,
+               'price_item_with_tax'=>$price_item_with_tax,
+               'price_item_without_tax'=>$price_item_without_tax,
+             ];
+          }),
+         // 'total_price_with_tax'=>doubleval($this->price),
           'products_count'=>$this->when(isset($this->products_count),function(){
               return $this->products_count;
           }),
