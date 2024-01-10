b9199b574fff12cffb6cdde6e56a36d9a176af37
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-10 03:58:04 +0200

    update order api

---
+++ app/Actions/SellerCustomOrdersClientsStatistics.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\custom_orders;
+use App\Models\orders;
+
+class SellerCustomOrdersClientsStatistics
+{
+    public static function get($user_id = null, $time_type = null){
+        $data = custom_orders::query()->whereHas('reply',function($e) use ($user_id){
+            $e->whereHas('custom_order_seller',function($q) use ($user_id){
+                $q->where('seller_id','=',$user_id);
+            });
+        })
+            ->join('payments','custom_orders.id','=','payments.paymentable_id')
+            ->where('payments.paymentable_type','=','App\Models\custom_orders')
+            ->selectRaw('custom_orders.* , payments.money');
+        return $data;
+    }
+}
--- app/Actions/SellerOrdersClientsStatistics.php
+++ app/Actions/SellerOrdersClientsStatistics.php
@@ -9,16 +9,16 @@ use App\Models\orders;
 class SellerOrdersClientsStatistics
 {
     public static function get($user_id = null, $time_type = null){
-    $clients = orders::query()
-        ->when($user_id != null , function ($e) use ($user_id){
-            $e->where('seller_id','=',$user_id);
-        })
-        ->when(auth()->check() && auth()->user()->role->name == 'seller' , function ($e) use ($user_id){
-            $e->where('seller_id','=',auth()->id());
-        })
-        ->join('payments','orders.id','=','payments.paymentable_id')
-        ->where('payments.paymentable_type','=','App\Models\orders')
-        ->selectRaw('orders.* , payments.money');
-    return $clients;
-}
+        $clients = orders::query()
+            ->when($user_id != null , function ($e) use ($user_id){
+                $e->where('seller_id','=',$user_id);
+            })
+            ->when(auth()->check() && auth()->user()->role->name == 'seller' , function ($e) use ($user_id){
+                $e->where('seller_id','=',auth()->id());
+            })
+            ->join('payments','orders.id','=','payments.paymentable_id')
+            ->where('payments.paymentable_type','=','App\Models\orders')
+            ->selectRaw('orders.* , payments.money');
+        return $clients;
+    }
 }
