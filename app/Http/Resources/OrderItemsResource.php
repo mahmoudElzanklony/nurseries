8b118c033961f0b70fe3cd262547f826a84d5f54
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-10 20:28:45 +0200

    add order info

--- app/Http/Resources/OrderItemsResource.php
+++ app/Http/Resources/OrderItemsResource.php
@@ -24,6 +24,7 @@ class OrderItemsResource extends JsonResource
 
     public function toArray($request)
     {
+        dd($this->payment);
         return [
           'id'=>$this->id,
           'product'=>ProductResource::make($this->whenLoaded('product')),
