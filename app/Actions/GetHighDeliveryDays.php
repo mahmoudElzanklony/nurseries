467ef489978d27c1f3f657e315a44c3553722b00
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-22 20:05:15 +0200

    update

--- app/Actions/GetHighDeliveryDays.php
+++ app/Actions/GetHighDeliveryDays.php
@@ -15,6 +15,7 @@ class GetHighDeliveryDays
             $new_data = collect($data)->map(function($e) use ($status){
                 return $e[$status];
             });
+            dd($new_data);
             return [
                 'days_delivery' => $new_data->max('days_delivery'),
                 'delivery_price' => $new_data->max('delivery_price'),
