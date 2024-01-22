d1b26fbac7256a441f6bb0f58094786fb5cb0efd
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-22 19:59:28 +0200

    update

--- app/Http/Resources/CustomOrderResource.php
+++ app/Http/Resources/CustomOrderResource.php
@@ -44,9 +44,7 @@ class CustomOrderResource extends JsonResource
            }),
            'selected_products'=>SelectedProductsResource::collection($this->whenLoaded('selected_products')),
            'delivery_data'=>$this->when($this->whenLoaded('selected_products'),function (){
-               $del = collect($this->selected_products)->map(function($e){
-                  return $e['details'];
-               });
+               $del = collect($this->selected_products);
                dd($del);
                return GetHighDeliveryDays::get($del);
            }),
