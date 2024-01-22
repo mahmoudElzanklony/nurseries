f7870a2f29911f83d403c12c272cb4047d7e0ece
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-22 20:07:08 +0200

    update

--- app/Actions/GetHighDeliveryDays.php
+++ app/Actions/GetHighDeliveryDays.php
@@ -4,22 +4,11 @@ namespace App\Actions;
 
 class GetHighDeliveryDays
 {
-    public static function get($data,$status = '')
+    public static function get($data)
     {
-        if($status == '') {
-            return [
-                'days_delivery' => $data->max('days_delivery'),
-                'delivery_price' => $data->max('delivery_price'),
-            ];
-        }else{
-            $new_data = collect($data)->map(function($e) use ($status){
-                return $e[$status];
-            });
-            dd($data[0]['reply']);
-            return [
-                'days_delivery' => $new_data->max('days_delivery'),
-                'delivery_price' => $new_data->max('delivery_price'),
-            ];
-        }
+        return [
+          'days_delivery'=>$data->max('days_delivery'),
+          'delivery_price'=>$data->max('delivery_price'),
+        ];
     }
 }
