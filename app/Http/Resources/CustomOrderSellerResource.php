81795715a453e8e1f3220fe214b7f8098599f70b
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-22 21:19:52 +0200

    update

--- app/Http/Resources/CustomOrderSellerResource.php
+++ app/Http/Resources/CustomOrderSellerResource.php
@@ -31,6 +31,7 @@ class CustomOrderSellerResource extends JsonResource
             $status = 'rejected_offer';
             $ar_status = 'تم رفض عرضك من قبل العميل';
         }
+        dd($this->status);
         return [
           'id'=>$this->id,
           'seller'=>UserResource::make($this->seller),
