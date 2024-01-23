27037bb346ff32238bdfca3115e7eac685f13921
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-23 23:52:06 +0200

    update

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -9,6 +9,7 @@ use App\Models\ai_questions;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 use Orhanerday\OpenAi\OpenAi;
 use Illuminate\Support\Facades\File;
@@ -20,13 +21,16 @@ class AIController extends Controller
     use messages;
     public function index(){
 
+
+        $this->delete_ai_images();
         // Create a new OpenAI client.
         $client = new OpenAi(env('openai'));
-        $prompt = "an image of a well-designed space, enriched with a variety of indoor plants suitable for any indoor setting, be it a home, office, or commercial area. Feature an assortment of plants like tall, leafy fiddle leaf figs for corners, small succulents for desks or shelves, and cascading spider plants for hanging planters. Include peace lilies and rubber plants, known for their air-purifying qualities, placed in strategic locations to enhance air quality and add a refreshing touch. The arrangement of the plants should be aesthetically pleasing, with a mix of sizes, textures, and colors, creating a vibrant yet harmonious environment. This imagery should capture the essence of integrating greenery into various indoor spaces, showcasing how plants can transform any area into a more inviting and lively place.";
-
+        //$prompt = "an image of a well-designed space, enriched with a variety of indoor plants suitable for any indoor setting, be it a home, office, or commercial area. Feature an assortment of plants like tall, leafy fiddle leaf figs for corners, small succulents for desks or shelves, and cascading spider plants for hanging planters. Include peace lilies and rubber plants, known for their air-purifying qualities, placed in strategic locations to enhance air quality and add a refreshing touch. The arrangement of the plants should be aesthetically pleasing, with a mix of sizes, textures, and colors, creating a vibrant yet harmonious environment. This imagery should capture the essence of integrating greenery into various indoor spaces, showcasing how plants can transform any area into a more inviting and lively place.";
+         $prompt = "Place various  plants ";
         if(request()->has('questions')){
             foreach(request('questions') as $q){
                 $question = ai_questions::query()->find($q['id']);
+
                 if($question != null){
                     $prompt .= 'and '.$question->en_name.' is '.$q['answer'].' ';
                     if($question->en_name == 'plant place'){
@@ -34,12 +38,12 @@ class AIController extends Controller
                             $trees = ['Shade trees ','Heat tolerant trees ','Evergreen trees ','Ornamental trees and plants ','Flowering trees ','Deciduous trees ','Fruit trees '
                                 ,'Seasonal flowers and plants ','Fragrant trees and plants ','Aromatic plants ','Flowering plants ','Climbing plants ','Rare plants '];
 
-                            $prompt .= 'and i want to select plants from these plants collection ( '.implode(' , ',$trees).' ) ';
+                            $prompt .= 'outdoor plants and i want to select plants from these plants collection ( '.implode(' , ',$trees).' ) ';
                         }else{
                             $trees = ['Hyacinthus orientalis','Aloe barbadensis','Lucky Bamboo','Kalanchoe blossfeldiana','Gardenia Jasminoides','Hedera','Scindapsus'
                                 ,'Ficus elastica Robusta','agave desmettiana','Ficus elastica Robusta','Rosa','Caladium','Mini Monstera','Epipremnum aureum'];
 
-                            $prompt .= 'and i want to select plants from these plants collection ( '.implode(' , ',$trees).' ) ';
+                            $prompt .= 'indoor plants and i want to select plants from these plants collection ( '.implode(' , ',$trees).' ) ';
                         }
                     }
                 }
@@ -49,6 +53,8 @@ class AIController extends Controller
             $prompt = request('prompt');
         }
 
+        $prompt .= " plants strategically around the room without any cropping or cutting, avoiding any modifications to the existing furniture. Enrich the environment with tall fiddle leaf figs in corners, small succulents on desks or shelves, and cascading spider plants in hanging planters. Ensure that the plants complement the current furniture layout.";
+
 
         try {
             $realPath = request()->file('image')->getRealPath();
@@ -57,6 +63,7 @@ class AIController extends Controller
                 null
             );
         }catch (\Exception $e){
+            //dd($e->getMessage());
             return messages::error_output($e);
         }
 
@@ -86,6 +93,15 @@ class AIController extends Controller
         return ProductQuestionResource::collection($data);
     }
 
+    public function delete_ai_images()
+    {
+        $files = $files = File::files('images/ai');
+        // Delete each file
+        foreach ($files as $file) {
+            File::delete($file);
+        }
+    }
+
     public function stability_ai($prompt,$original,$mask)
     {
 
@@ -106,6 +122,7 @@ class AIController extends Controller
                 "samples" => 10,
                 "steps" => 30
             ];
+
             $response =  Http::withHeaders($headers)
                 ->attach('init_image', $original)
                 ->attach('mask_image', $mask)
@@ -129,8 +146,7 @@ class AIController extends Controller
 
 
 
-        File::deleteDirectory(public_path('/images/ai'));
-        mkdir(public_path('/images/ai'));
+
         try {
             foreach ($response->json()['artifacts'] as $key => $img) {
                 $image = $img['base64'];  // your base64 encoded
