c2ea14fc54c25f1e7176646b3a4e809d134abb6b
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-25 01:55:09 +0200

    update

--- app/Http/Controllers/SellerInfoController.php
+++ app/Http/Controllers/SellerInfoController.php
@@ -89,7 +89,7 @@ class SellerInfoController extends Controller
         $output = $obj->get_profit('App\Actions\SellerOrdersClientsStatistics',null,'money',$time_type,[],'orders.created_at','sum');
         $output_two = $obj->get_profit('App\Actions\SellerCustomOrdersClientsStatistics',null,'money',$time_type,[],'custom_orders.created_at','sum');
         $final = [];
-        dd($output[0]['placeholder']);
+        dd(Carbon::now()->toDateString());
         foreach($output as $key => $item){
             $info = [];
             //dd($item['placeholder']);
