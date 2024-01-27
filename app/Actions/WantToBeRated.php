1b7c6d66ba14a2ee03b4e9d08e144d8206b5aaa1
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-27 07:09:05 +0200

    update

--- app/Actions/WantToBeRated.php
+++ app/Actions/WantToBeRated.php
@@ -11,15 +11,15 @@ class WantToBeRated
 {
     public static function check($product_id){
         if(auth()->check()) {
-            $order = orders_items_rates::query()
+            /*$order = orders_items_rates::query()
                 ->where('user_id','=',auth()->id())
                 ->whereHas('order_item',function($e) use ($product_id){
                 $e->where('product_id','=',$product_id);
-            })->first();
-           /* $order = orders_items::query()->whereHas('order',function($e){
+            })->first();*/
+            $order = orders_items::query()->whereHas('order',function($e){
                 $e->where('user_id','=',auth()->id());
             })->where('product_id','=',$product_id)
-              ->whereDoesntHave('rate')->first();*/
+              ->whereDoesntHave('rate')->first();
 
             if($order == null){
                 return true;
