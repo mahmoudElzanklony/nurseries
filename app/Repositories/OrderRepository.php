2fa40c4ba9a8cfc798337a6bb77f49033c67068b
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-27 05:28:19 +0200

    update

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -153,7 +153,7 @@ class OrderRepository
                     // handle final price
                     //echo 'quantity ==>'.$item['quantity'] .'<br>';
                     $final_price = $this->handle_final_price($product,$whole_price,$discount,'product',$item['quantity']);
-                    dd($final_price);
+                    dd($discount);
                     $this->order_total_price += $final_price;
                     //echo 'price of product'.$final_price.' and final now =====> '.$this->order_total_price .'<br>';
                     $order_item = orders_items::query()->create([
