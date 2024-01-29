d7544b1bbb2bde5192c6971b08da1ef4fd4eaaaa
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-29 02:08:26 +0200

    update

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -84,6 +84,7 @@ class OrdersController extends Controller
         if($check_err_delivery['error'] > 0){
             return messages::error_output(trans('keywords.seller').' ( '.$seller->username.' ) '.trans('keywords.dont_support_delivery_product').' ( '.$check_err_delivery['product_name'].' ) '.trans('keywords.to_default_address'),401);
         }
+        dd('abc');
         if(true){
             // the visa is okay now
 
