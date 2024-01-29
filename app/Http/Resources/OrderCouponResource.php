4e004f350b8b563d59b4cabe7a23c6b2e66af05a
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-29 02:22:54 +0200

    update

--- app/Http/Resources/OrderCouponResource.php
+++ app/Http/Resources/OrderCouponResource.php
@@ -14,7 +14,7 @@ class OrderCouponResource extends JsonResource
      */
     public function toArray($request)
     {
-        dd($this->toArray());
+        dd($this->toArray($request));
         return [
             'data'=>CheckCouponResource::collection($this->data),
             'coupon'=>CouponRessource::make($this->coupon),
