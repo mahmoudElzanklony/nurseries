50ab9f3086e4a9e65c9c4b9628d21e2d95f2b4fd
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-29 02:12:55 +0200

    update

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -15,6 +15,7 @@ use App\Filters\StartDateFilter;
 use App\Filters\EndDateFilter;
 use App\Http\Requests\ordersFormRequest;
 use App\Http\Resources\CheckCouponResource;
+use App\Http\Resources\OrderCouponResource;
 use App\Http\Resources\OrderItemsResource;
 use App\Http\Resources\OrderResource;
 use App\Http\Resources\OrderShipmentsInfo;
@@ -131,9 +132,12 @@ class OrdersController extends Controller
                 array_push($final,$obj);
             }
 
+            $last_final = [
+                'data'=>$final,
+                'coupon'=> $coupon_repos->coupon
+            ];
 
-
-            return CheckCouponResource::collection($final);
+            return OrderCouponResource::collection($last_final);
         }else{
             return messages::error_output('بيانات الفيزا الخاصه بك خاطئة يرجي مراجعتها من فضلك');
         }
---
+++ app/Http/Resources/OrderCouponResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderCouponResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+          'data'=>CheckCouponResource::collection($this->data),
+          'coupon'=>CouponRessource::make($this->coupon),
+        ];
+    }
+}
