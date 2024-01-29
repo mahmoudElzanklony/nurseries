834168b33f644ba4bfdd6cf2ffd58bcfe2953210
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-29 02:43:14 +0200

    update

--- app/Http/Resources/OrderCouponResource.php
+++ app/Http/Resources/OrderCouponResource.php
@@ -14,15 +14,6 @@ class OrderCouponResource extends JsonResource
      */
     public function toArray($request)
     {
-        $data = $this->resource['data'] ?? null; // Access 'data' key from the $final_result array
-        $coupon = $this->resource['coupon'] ?? null; // Access 'coupon' key from the $final_result array
-
-        // Use $data and $coupon as needed in your response
-        // ...
-
-        return [
-            'data' => $data,
-            'coupon' => $coupon,
-        ];
+        return parent::toArray($request);
     }
 }
