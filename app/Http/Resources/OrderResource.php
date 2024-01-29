a98e198bc7a4572f46169ae8f6a2061cc7f50920
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-01-29 03:34:53 +0200

    update

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -30,8 +30,7 @@ class OrderResource extends JsonResource
               $item =  orders_items::query()->where('order_id','=',$this->id)->has('coupon')->with('coupon')->first();
 
               if($item != null){
-                  dd($item);
-                  $coupon = coupons::withTrashed()->find($item->coupon->couponable_id);
+                  $coupon = coupons::withTrashed()->find($item->coupon->coupon_id);
                   if($coupon != null) {
                       return CouponRessource::make($coupon);
                   }
