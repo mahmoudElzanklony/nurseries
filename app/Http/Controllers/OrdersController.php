baad9cf9c9e3bd4cbc04210254364546d7064b3d
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-08-04 00:11:46 +0300

    add order noon payment billing user

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -50,8 +50,7 @@ class OrdersController extends Controller
         $seller = User::query()->find(request('seller_id'));
         // check if this of any these products any one that has no delivery way to default client address
         $check_err_delivery = $order_repo->check_delivery_products($data['items']);
-        //if($check_err_delivery['error'] > 0){
-        if(false){
+        if($check_err_delivery['error'] > 0){
             return messages::error_output(trans('keywords.seller').' ( '.$seller->username.' ) '.trans('keywords.dont_support_delivery_product').' ( '.$check_err_delivery['product_name'].' ) '.trans('keywords.to_default_address'),401);
         }
         if($order_repo->validate_payment_info($data['payment_data'])['status'] == true){
@@ -78,6 +77,22 @@ class OrdersController extends Controller
                     "name" => "Mraken Noon payment",
                     "items"=>$order_repo->noon_items_format
                 ],
+                "billing"=> [
+                    "address"=> [
+                        "street"=> $default_address>address,
+                        "city"=>"",
+                        "stateProvince"=> "arabia sudia",
+                        "country"=> "SA",
+                        "postalCode"=> "12345"
+                    ],
+                    "contact"=> [
+                        "firstName"=> auth()->user()->username,
+                        "lastName"=> "",
+                        "phone"=> auth()->user()->phone,
+                        "mobilePhone"=> auth()->user()->phone,
+                        "email"=> auth()->user()->email
+                    ]
+                 ],
                 "configuration" => [
                     "locale" => "ar"
                 ]
